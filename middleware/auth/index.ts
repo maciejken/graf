22824a04f85b06054e598e9ca92c1948048c0baa
Maciej Kędra <maciej.kedra@capgemini.com>
2024-01-12 12:20:43 +0100

    add auth logs

--- middleware/auth/index.ts
+++ middleware/auth/index.ts
@@ -6,11 +6,17 @@ export function ensureAuthenticated(
   _res: Response,
   next: NextFunction
 ) {
-  const { remoteAddress } = req.socket;
-  const isAuthenticated =
-    !remoteAddress || whitelistedIps?.includes(remoteAddress);
+  const isAuthenticated = whitelistedIps?.includes(req.ip);
 
   if (isAuthenticated) {
+    console.log("req.ip", req.ip);
+    console.log("req.connection.remoteAddress", req.connection.remoteAddress);
+    console.log("req.socket.remoteAddress", req.socket.remoteAddress);
+    console.log(
+      "req.headers['x-forwarded-for']",
+      req.headers["x-forwarded-for"]
+    );
+    console.log("req.ips", req.ips);
     next();
   } else {
     next(new Error(`Unauthenticated: ${req.socket.remoteAddress}`));
