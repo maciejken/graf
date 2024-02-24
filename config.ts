c860eb1477a463988e89d3b939b0ae62603f69fc
maciejken <maciejken@gmail.com>
2024-02-24 13:20:51 +0100

    update config log

--- config.ts
+++ config.ts
@@ -6,10 +6,10 @@ export const relyingPartyName = Deno.env.get("RELYING_PARTY_NAME");
 export const expectedOrigin = Deno.env.get("EXPECTED_ORIGIN");
 export const privateKey = Deno.env.get("PRIVATE_KEY");
 export const registrationTokenExpiresIn = Deno.env.get(
-  "REGISTRATION_TOKEN_EXPIRES_IN"
+  "REGISTRATION_TOKEN_EXPIRES_IN",
 );
 export const authenticationTokenExpiresIn = Deno.env.get(
-  "AUTHENTICATION_TOKEN_EXPIRES_IN"
+  "AUTHENTICATION_TOKEN_EXPIRES_IN",
 );
 export const genericTokenExpiresIn = Deno.env.get("GENERIC_TOKEN_EXPIRES_IN");
 
@@ -23,17 +23,21 @@ export function checkConfig() {
     "REGISTRATION_TOKEN_EXPIRES_IN",
     "AUTHENTICATION_TOKEN_EXPIRES_IN",
     "GENERIC_TOKEN_EXPIRES_IN",
-    "PRIVATE_KEY"
+    "PRIVATE_KEY",
   ]
     .map((key: string) => ({ key, value: Deno.env.get(key) }))
     .filter(({ value }) => !value);
 
   if (emptyValues.length) {
-    const msg = `Some environment variables are undefined: ${emptyValues
-      .map(({ key }) => key)
-      .join(", ")}`;
+    const msg = `Some environment variables are undefined: ${
+      emptyValues
+        .map(({ key }) => key)
+        .join(", ")
+    }`;
     throw new Error(msg);
   }
+
+  console.debug("Environment variables are ok");
 
   return true;
 }
